2bfa17310b5af960c2b9fab268396d884b7184a6
Marcin Szewczyk <platatita@gmail.com>
2022-07-29 19:58:34 +0200

    Update to edition 2021

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
-#[cfg(test)]
-extern crate quickcheck;
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
-#[cfg(test)]
-extern crate regex;
-
 use std::collections::BTreeMap;
 
-#[macro_use] mod ion;
+#[macro_use]
+mod ion;
 mod parser;
 mod writer;
-pub use parser::{ Parser, ParserError };
+pub use parser::{Parser, ParserError};
 pub use writer::Writer;
 
 pub type Dictionary = BTreeMap<String, Value>;
-pub use ion::{Ion, IonError, FromIon, Section, Value};
+pub use self::ion::{FromIon, Ion, IonError, Section, Value};
 pub type Row = Vec<Value>;
