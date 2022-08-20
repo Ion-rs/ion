4a203bcf9c81c2dcaba5115a8727f116948a63bc
Marcin Szewczyk <platatita@gmail.com>
2022-08-20 19:25:55 +0200

    Remove 'Writer'

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@ use std::collections::BTreeMap;
 #[macro_use]
 mod ion;
 mod parser;
-mod writer;
 
 pub use self::ion::{FromIon, Ion, IonError, Section, Value};
 pub use self::parser::{Parser, ParserError};
-pub use self::writer::Writer;
 
 pub type Dictionary = BTreeMap<String, Value>;
 pub type Row = Vec<Value>;
